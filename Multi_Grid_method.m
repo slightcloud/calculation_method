05d23a4ec0040f0315f016b9666b4d2aa9f6274c
slightcloud <32709628+slightcloud@users.noreply.github.com>
2018-05-27 18:32:34 +0800

    更新为1D多格点法求解电势的完整算法

--- Multi_Grid_method.m
+++ Multi_Grid_method.m
@@ -28,22 +28,22 @@ R_c = zeros(ni_c,1);
 % eps_c = zeros(ni_c,1);
 eps_f = zeros(ni,1);
 phi = zeros(ni,1);
-
+inner_its = 1;   %fine grid iteration time
+inner2_its = 50; %coarse grid iteration time
+w = 1.4;         %sor factor
 %start iteration
 for it = 1:solver_max
     
-   inner_its = 1;   %fine grid iteration time
-   inner2_its = 50; %coarse grid iteration time
-   w = 1.4;         %sor factor
+
 %    phi = zeros(ni,1);
    % 1) perform one or more iterations on fine mesh
-   phi(1,1) = phi(2,1);    %Neuman boundary condition
-   for i = 2:ni-1
-       
-       g = 0.5*(phi(i-1,1)+phi(i+1,1)-dx*dx*b(i,1));
-       phi(i,1) = phi(i,1)+w*(g-phi(i,1));
+   for n = 1:inner_its
+       phi(1,1) = phi(2,1);    %Neuman boundary condition
+       for i = 2:ni-1
+           g = 0.5*(phi(i-1,1)+phi(i+1,1)-dx*dx*b(i,1));
+           phi(i,1) = phi(i,1)+w*(g-phi(i,1));
+       end
    end
-    
    % 2) compute residue on the fine mesh, R = A*phi-b
    R_f(2:ni-1,1) = (phi(1:ni-2,1)-2*phi(2:ni-1,1)+phi(3:ni,1))/(dx*dx)-b(2:ni-1,1);
    R_f(1,1) = (phi(1,1)-phi(2,1))/dx;  %neumann boundary
@@ -63,26 +63,28 @@ for it = 1:solver_max
        hold off
        break;
    end
-   fprintf("norm = %f\n",norm);
+   fprintf("norm = %g\n",norm);
    
-%    % 3) restrict residue to the coarse mesh
-%    R_c(0.5*((3:2:ni-2)+1),1) = 0.25*(R_f(2:2:ni-3,1)+R_f(3:2:ni-2,1)+R_f(4:2:ni-1,1));
-%    R_c(1,1) = R_f(1,1);
-%    
-%    % 4) perform few iteration of the correction vector on the coarse mesh
-%    for n = 1:inner2_its
-%        eps_c = zeros(ni_c,1);
-%        eps_c(1,1) = eps_c(2,1)+dx_c*R_c(1,1);
-%        g = 0.5*(eps_c(1:ni_c-2,1)+eps_c(3:ni_c,1)-dx_c*dx_c*R_c(2:ni_c-1,1));
-%        eps_c(2:ni_c-1,1) = eps_c(2:ni_c-1,1)+w*(g-eps_c(2:ni_c-1,1));
-%    end
-%    
-%    % 5) interpolate eps to fine mesh
-%    eps_f(3:2:ni-2,1) = eps_c(0.5*((3:2:ni-2)+1),1);
-%    eps_f(2:2:ni-1,1) = 0.5*(eps_c(0.5*(2:2:ni-1),1)+eps_c(0.5*(2:2:ni-1)+1,1));
-%    eps_f(1,1) = eps_c(1,1);
-%    % 6) update solution on the fine mesh
-%    phi(1:ni-1,1) = phi(1:ni-1,1)-eps_f(1:ni-1,1);
+   % 3) restrict residue to the coarse mesh
+   R_c(0.5*((3:2:ni-2)+1),1) = 0.25*(R_f(2:2:ni-3,1)+2*R_f(3:2:ni-2,1)+R_f(4:2:ni-1,1));
+   R_c(1,1) = R_f(1,1);
+   
+   % 4) perform few iteration of the correction vector on the coarse mesh
+   eps_c = zeros(ni_c,1);
+   for n = 1:inner2_its
+       eps_c(1,1) = eps_c(2,1)+dx_c*R_c(1,1);
+       for i = 2:ni_c-2
+           g = 0.5*(eps_c(i-1,1)+eps_c(i+1,1)-dx_c*dx_c*R_c(i,1));
+           eps_c(i,1) = eps_c(i,1)+w*(g-eps_c(i,1));
+       end
+   end
+   
+   % 5) interpolate eps to fine mesh
+   eps_f(3:2:ni-2,1) = eps_c(0.5*((3:2:ni-2)+1),1);
+   eps_f(2:2:ni-1,1) = 0.5*(eps_c(0.5*(2:2:ni-1),1)+eps_c(0.5*(2:2:ni-1)+1,1));
+   eps_f(1,1) = eps_c(1,1);
+   % 6) update solution on the fine mesh
+   phi(1:ni-1,1) = phi(1:ni-1,1)-eps_f(1:ni-1,1);
    
    
    
